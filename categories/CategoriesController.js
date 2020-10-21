6a5883162d4b0155fc253dec908b7beb70a00848
Joao Pedro <802.018@alunos.unigran.br>
2020-10-21 14:21:21 -0300

    Concluindo Categories e add o slug

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugfy");
 
-
-router.get("/categories",(req,res)=>{
-    res.send("ROTA DE CATEGORIA");
-});
 
 router.get("/admin/categories/new",(req,res)=>{
-    res.send("ROTA PARA UMA NOVA CATEGORIA");
+  res.render("admin/categories/new.ejs");
+});
+
+router.post("/categories/save",(req,res)=>{
+    var title = req.body.title;
+    if(title!=undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(()=>{
+            res.redirect("/");
+        })
+
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+
 });
 
 module.exports = router;
