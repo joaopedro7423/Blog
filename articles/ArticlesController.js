d6145f7511956715d73af878a9940f227cb036ce
Joao Pedro <802.018@alunos.unigran.br>
2020-10-22 17:03:04 -0300

    Paginação

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -70,6 +70,81 @@ router.post("/articles/delete",(req,res)=>{
     }
 });
 
-//
+//Editar Artigos
+
+router.get("/admin/articles/edit/:id",(req,res)=>{
+
+    var id =req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+
+            Category.findAll().then(categories =>{
+                res.render("admin/articles/edit.ejs",{article: article, categories:categories});
+
+            })
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+
+
+});
+
+//salvar as alterações
+router.post("/articles/update",(req,res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({title:title, body:body, categoryId: category, slug:slugify(title)},
+        {where:{
+            id:id
+        }}).then(()=>{
+            res.redirect("/admin/articles"); 
+        })
+});
+
+
+//controle da paginação
+router.get("/articles/page/:num",(req,res)=>{
+    var page = req.params.num;
+    var offset = 0;
+
+    if(isNaN(page) || page == 1){
+        offset =0;
+    }else{
+        offset = parseInt(page)*4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+    }).then(articles =>{
+
+
+
+        var next;
+        if(offset +4 >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+
+        var result ={
+            next: next,
+            articles: articles 
+        }
+
+        Category.findAll().then(categories =>{
+            res.render("admin/articles/page.ejs",{result: result, categories: categories})
+        })
+
+        res.json(result);
+    })
+
+});
 
 module.exports = router;
